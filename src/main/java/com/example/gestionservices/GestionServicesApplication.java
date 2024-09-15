f03d9bbbab26db48138cfbfdfe8fcdc4becc39eb
najia <najiaknizi199@gmail.com>
2024-09-16 00:09:22 +0100

    application properities

---
+++ src/main/java/com/example/gestionservices/GestionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionservices/GestionServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
